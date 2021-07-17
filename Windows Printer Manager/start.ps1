2f56bd55850fed00199cd9c20c6154668a4c60d8
joshgallantt <77059265+joshgallantt@users.noreply.github.com>
2021-07-17 18:13:09 +0100

    Update start.ps1

--- Windows Printer Manager/start.ps1
+++ Windows Printer Manager/start.ps1
@@ -1,4 +1,62 @@
 PowerShell -NoProfile -ExecutionPolicy Unrestricted -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Unrestricted -File ""start.ps1""' -Verb RunAs}";
+$Title = "Printer Manager by Josh"
+$host.UI.RawUI.WindowTitle = $Title
+
+
+<# ADD NEW PRINTERS HERE #>
+
+
+<# If you're unsure where to get installed printer driver files, "C:\Windows\System32\DriverStore\FileRepository" #>
+
+$Printer1 = [Printer]::new();
+$Printer1.id = "1"
+$Printer1.name = "Example Printer 1"
+$Printer1.location = "Third Floor Office"
+$Printer1.address = "203.0.113.12"
+$Printer1.driver = "HP Color LaserJet Pro M252 PCL 6"
+$Printer1.driverlocation = "C:\Users\$env:userprofile\Desktop\Windows Printer Manager\Drivers\Printer 1\hpor3c2a_x64.inf"
+
+$Printer2 = [Printer]::new();
+$Printer2.id = "2"
+$Printer2.name = "Example Printer 2"
+$Printer2.location = "Fourth Floor Office"
+$Printer2.address = "203.0.113.11"
+$Printer2.driver = "HP Color LaserJet Pro M252 PCL 6"
+$Printer2.driverlocation = "C:\Users\$env:userprofile\Desktop\Windows Printer Manager\Drivers\Printer 1\hpor3c2a_x64.inf"
+
+$Printer3 = [Printer]::new();
+$Printer3.id = "3"
+$Printer3.name = "Example Printer 3"
+$Printer3.location = "Fifth Floor Office"
+$Printer3.address =  "203.0.113.13"
+$Printer3.driver = "YOU FIND THIS IN THE .INF FILE"
+$Printer3.driverlocation = "U:\Drivers\Printer3_Driver\Epson.inf"
+
+<# MAKE SURE YOU ADD THEM TO THE LIST #>
+
+$printerslist = $Printer1, $Printer2, $Printer3
+
+<# AND TO THE SELECTION CHOICES #>
+
+function printer-choices{
+    $selection = Read-Host
+        switch ($selection){
+            '1' {
+                printer-handler $Printer1 $type
+                }
+            '2' {
+                printer-handler $Printer2 $type
+                }
+            '3' {
+                printer-handler $Printer3 $type
+                }
+            'm' {main-menu}
+            'q' {exit}
+        }
+}
+
+<# No need to go paseed here! :D #>
+
 
 class Printer {
     [string]$id;
@@ -10,7 +68,6 @@ class Printer {
     [bool] $installed = $false;
 
     [void] install(){
-        Write-Output("The following GSHQ printers can be uninstalled on this device:`n")
         Foreach ($printer in  Get-Printer){
             if($printer.name -eq $this.name){
                 Remove-Printer -Name $this.name
@@ -38,100 +95,73 @@ class Printer {
     }
 }
 
+
 function main-menu{
     cls
 
-    $someinstalled = $false
     Foreach ($installedprinter in  Get-Printer){
-        Foreach ($printer in  $printerslist){
+        Foreach ($printer in $printerslist){
             if($printer.Name -eq $installedprinter.Name){
                 $printer.installed = $true
-                $someinstalled = $true
+                $installed = $true
             }
         }
     }
 
     Write-Output(" ======================= PRINTER MANAGER =======================`n")
 
-    if(!$someinstalled){
-        Write-Output("There are currently no GSHQ printers installed on this device.`n")
-        $selection = Read-Host "(1) install a printer`n(Q) quit`n`nSelect an Option"
+    if(!$installed){
+        Write-Host("There are currently no GSHQ printers installed on this device.`n") -ForegroundColor "Red"
+        Write-Host "Select an option:"
+        Write-Host "  (1) Add a printer" -ForegroundColor "Yellow"
+        Write-Host "  (Q) To Quit" -ForegroundColor "Yellow"
+        $selection = Read-Host
         switch ($selection){
-        '1' {install-menu "Install"} 
+        '1' {install-menu "install"} 
         'q' {exit}
         }
     }
     
     else{
         Write-Output("The following GSHQ printers are installed on this device:`n")
-        Foreach ($printer in  $printerslist){
+        Foreach ($printer in $printerslist){
             if($printer.installed){
-                Write-Output "$($printer.name), $($printer.location), $($printer.address), $($printer.driver)`n"
+                Write-Host("  $($printer.name), $($printer.location), $($printer.address), $($printer.driver)`n") -ForegroundColor "Green"
+            
             }
-            $selection = Read-Host "(1) install/reinstall a printer`n(2) uninstall a printer`n(Q) quit`n`nSelect an Option"
-            switch ($selection){
-                '1' {install-menu "Install"} 
-                '2' {install-menu "Uninstall"} 
-                'q' {exit}
-            }
+        }
+
+        Write-Host "Select an option:"
+        Write-Host "  (1) Add a printer" -ForegroundColor "Yellow"
+        Write-Host "  (2) Remove a printer" -ForegroundColor "Yellow"
+        Write-Host "  (Q) To Quit" -ForegroundColor "Yellow"
+        $selection = Read-Host
+        switch ($selection){
+            '1' {install-menu "install"} 
+            '2' {install-menu "uninstall"} 
+            'q' {exit}
         }
     }
 }
 
 function printer-handler($printerobject, [string]$method){
     cls
-    Write-Output("$($method)ing $($method.name) please wait... ")
+
+    Write-Host("  $($method)ing $($printerobject.name) please wait... this can a few minutes") -ForegroundColor "Red"
         if($method -eq "Install"){
             $printerobject.Install()
         }
 
         else{
-        $printerobject.Install()
+            $printerobject.Uninstall()
         }
+    cls
+    Write-Host("`n   $($printerobject.name) $($method)ed!") -ForegroundColor "Green"
+    Start-Sleep -s 1
+    Write-Host("   Returning to Main Menu...")
+    Start-Sleep -s 1
     main-menu
-    Start-Sleep -s 3
 }
-
-<#
-
-No need to edit anything above.
-
-Below is where you add new printer objects to the $printerslist array (make sure you also add any new printers in the install/uninstall function)
-
-If you're unsure where to get installed printer driver files, "C:\Windows\System32\DriverStore\FileRepository"
-
-#>
-
-
-$Printer1 = [Printer]::new();
-$Printer1.id = "1"
-$Printer1.name = "Example Printer 1"
-$Printer1.location = "Third Floor Office"
-$Printer1.address = " 203.0.113.11"
-$Printer1.driver = "HP Color LaserJet Pro M252 PCL 6"
-$Printer1.driverlocation = "C:\Users\$env:userprofile\Desktop\Windows Printer Manager\Drivers\Printer 1\hpor3c2a_x64.inf"
-
-$Printer2 = [Printer]::new();
-$Printer2.id = "2"
-$Printer2.name = "Example Printer 2"
-$Printer2.location = "Fourth Floor Office"
-$Printer2.address = "203.0.113.11"
-$Printer2.driver = "YOU FIND THIS IN THE .INF FILE"
-$Printer2.driverlocation = "U:\Drivers\Printer2_Driver\HP.inf"
-
-$Printer3 = [Printer]::new();
-$Printer3.id = "3"
-$Printer3.name = "Example Printer 3"
-$Printer3.location = "Fifth Floor Office"
-$Printer3.address =  "203.0.113.11"
-$Printer3.driver = "YOU FIND THIS IN THE .INF FILE"
-$Printer3.driverlocation = "U:\Drivers\Printer3_Driver\Epson.inf"
-
-$printerslist = $Printer1, $Printer2, $Printer3
-
-
-$Title = "Printer Manager by Josh"
-$host.UI.RawUI.WindowTitle = $Title
 
 function install-menu([string]$type){
     cls
@@ -140,32 +170,30 @@ function install-menu([string]$type){
 
     Write-Output("The following GSHQ printers can be $($type)ed on this device:`n")
 
-    if($type = "Uninstall"){
+    if($type -eq "uninstall"){
         Foreach ($printer in  $printerslist){
             if($printer.installed){
-                $printer
+                Write-Host("  ($($printer.id))") -NoNewLine -ForegroundColor "Yellow"
+                Write-Host("  $($printer.name), $($printer.location), $($printer.address), $($printer.driver)`n") -ForegroundColor "Red"
             }
         }
     }
 
     else{
-        $printerslist
+        Foreach ($printer in  $printerslist){
+            if(!$printer.installed){
+                Write-Host("  ($($printer.id))") -NoNewLine -ForegroundColor "Yellow"
+                Write-Host("  $($printer.name), $($printer.location), $($printer.address), $($printer.driver)`n") -ForegroundColor "Green"
+            }
+        }
     }
 
-    $selection = Read-Host "`nEnter the ID of the printer you wish to $($type), (m) for main menu, or (q) to exit"
-        switch ($selection){
-            '1' {
-                printer-handler $Printer1 $type
-                }
-            '2' {
-                printer-handler $Printer2 $type
-                }
-            '3' {
-                printer-handler $Printer3 $type
-                }
-            'm' {main-menu}
-            'q' {exit}
-        }
+    Write-Host "Select an option:"
+    Write-Host "  (num) the printer you wish to $($type)" -ForegroundColor "Yellow"
+    Write-Host "  (M) for main menu" -ForegroundColor "Yellow"
+    Write-Host "  (Q) to exit" -ForegroundColor "Yellow"
+    printer-choices
+
 }
 
 main-menu
